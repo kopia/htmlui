78863c13e36fb5c8bfca62d668e2490b91082ccf
Jarek Kowalski <jaak@jkowalski.net>
2021-12-18 13:10:36 -0800

    show changes on push_build.sh even without ability to push (#7)

--- push_build.sh
+++ push_build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
+tmpdir=$(mktemp -d)
+git clone https://$GITHUB_TOKEN@github.com/kopia/htmluibuild $tmpdir
+rm -rf $tmpdir/build
+cp -rv build $tmpdir
+
+echo Changes
+(cd $tmpdir && git diff)
+
 if [ "$GITHUB_TOKEN" == "" ]; then
   echo GITHUB_TOKEN not set, not pushing the build.
   exit 0
 fi
 
-tmpdir=$(mktemp -d)
-git clone https://$GITHUB_TOKEN@github.com/kopia/htmluibuild $tmpdir
-rm -rf $tmpdir/build
-cp -rv build $tmpdir
 (cd $tmpdir && git add -A && git -c "user.name=Kopia Builder" -c "user.email=builder@kopia.io" commit -m "HTMLUI update for $GITHUB_SHA" && git push)
