3754d2f04af80f9af2a865478a48829a1c8cfc45
Jarek Kowalski <jaak@jkowalski.net>
2021-12-23 10:23:38 -0800

    ui: added dark mode persistence using /api/v1/ui-preferences (#12)

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,24 +1,25 @@
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-// TODO - this is very rudimentary, we still need to:
-// - match OS preference by default
-// - persist the choice
-export function ToggleDarkModeButton() {
-    const [v, setV] = useState(0);
-  
-    const h = document.querySelector("html");
-    const isDark = h.className === "dark";
-    
-    function setMode(mode) {
-      h.className = mode;
-  
-      setV(v + 1);
+export function ToggleDarkModeButton(props) {
+  let theme = props.theme;
+  if (!theme) {
+    theme = "light";
+
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      // browser supports light/dark mode and user prefers dark theme.
+      theme = "dark"
     }
+  }
+
+  const onChangeTheme = props.onChangeTheme;
+
+  // keep html class in sync with button state.
+  const h = document.querySelector("html");
+  h.className = theme;
   
-    return isDark ? <Button variant="outline-light" size="sm" title="Switch to Light Mode" onClick={() => setMode("light")}><FontAwesomeIcon icon={faSun} />
-     </Button> : <Button variant="outline-dark" size="sm" title="Switch to Dark Mode" onClick={() => setMode("dark")}><FontAwesomeIcon icon={faMoon} /></Button>;
+  return theme === "dark" ? <Button variant="outline-light" size="sm" title="Switch to Light Mode" onClick={() => onChangeTheme("light")}><FontAwesomeIcon icon={faSun} />
+    </Button> : <Button variant="outline-dark" size="sm" title="Switch to Dark Mode" onClick={() => onChangeTheme("dark")}><FontAwesomeIcon icon={faMoon} /></Button>;
 }
-  
