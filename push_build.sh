2516eb52a2603d40c6b8c2e9433cef9f9c288c82
Jarek Kowalski <jaak@jkowalski.net>
2022-01-03 09:35:46 -0800

    ci: auto-link to source commit (#17)

--- push_build.sh
+++ push_build.sh
@@ -14,4 +14,4 @@ if [ "$GITHUB_TOKEN" == "" ]; then
   exit 0
 fi
 
-(cd $tmpdir && git add -A && git -c "user.name=Kopia Builder" -c "user.email=builder@kopia.io" commit -m "HTMLUI update for $GITHUB_SHA" && git push)
+(cd $tmpdir && git add -A && git -c "user.name=Kopia Builder" -c "user.email=builder@kopia.io" commit -m "HTMLUI update for $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" && git push)
