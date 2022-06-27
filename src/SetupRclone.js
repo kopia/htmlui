554ea2186f9a14fdb77796c05c5c73da0bab06ca
basldfalksjdf <106879934+basldfalksjdf@users.noreply.github.com>
2022-06-26 22:16:31 -0400

    Clarifying language in SetupRclone.js

--- src/SetupRclone.js
+++ src/SetupRclone.js
@@ -19,10 +19,19 @@ export class SetupRclone extends Component {
     render() {
         return <>
             <Row>
+                Rclone is a third-party program that allows you to manage files on cloud storage. Kopia allows you to backup to all storage providers that Rclone supports, but please note that Rclone support is experimental. In theory, all Rclone-supported storage providers should work with Kopia. However, in practice, only OneDrive and Google Drive have been tested to work with Kopia through Rclone.
+            </Row>
+            <Row>
+                You need to do the following BEFORE you try to use Rclone with Kopia: (1) download Rclone from <a href="https://rclone.org/" target="_blank">https://rclone.org/</a> and (2) configure Rclone to setup a remote to the storage provider you want to use Kopia with: see <a href="https://rclone.org/docs/" target="_blank">https://rclone.org/docs/</a>. Once you have configured a Rclone remote, you have to create a Kopia repository for that Rclone remote by entering below (i) the path to the Rclone remote and (ii) the path to the Rclone executable you have on your machine. Once you have setup this repository, Kopia will handle automatically launch Rclone when Rclone is needed, but you need to make sure to keep Rclone up-to-date -- see <a href="https://rclone.org/commands/rclone_selfupdate/" target="_blank">https://rclone.org/commands/rclone_selfupdate/</a>.
+            </Row>
+            <Row>
+                <Col>&nbsp;</Col>
+            </Row>
+            <Row>
                 {RequiredField(this, "Rclone Remote Path", "remotePath", { autoFocus: true, placeholder: "enter <name-of-rclone-remote>:<path>" })}
             </Row>
             <Row>
-                {OptionalField(this, "rclone executable", "rcloneExe", { placeholder: "enter path to rclone executable" })}
+                {OptionalField(this, "Rclone Executable Path", "rcloneExe", { placeholder: "enter path to rclone executable" })}
             </Row>
         </>;
     }
