f71758a26cb3a66ac07be284d34a14f548568e25
basldfalksjdf <106879934+basldfalksjdf@users.noreply.github.com>
2022-07-02 11:48:36 -0400

    Clarifying language for SetupGCS.js (#74)
    * Clarifying language for SetupGCS.js
    
    * Update SetupGCS.js
    
    * Update SetupGCS.js
    
    * Update SetupGCS.js

--- src/SetupGCS.js
+++ src/SetupGCS.js
@@ -20,6 +20,12 @@ export class SetupGCS extends Component {
     render() {
         return <>
             <Row>
+                To use Google Cloud Storage with Kopia, you need a JSON credentials file that gives Kopia access to upload to your Google Cloud Storage account. The easiest way to generate such a file is by using a Google Cloud Storage Service account. If you have trouble generating this credentials file, you can alternatively connect to Google Cloud Storage through Kopia's Amazon S3 option, because Google Cloud Storage is S3 compatible. See <a href="https://kopia.io/docs/repositories/#google-cloud-storage" target="_blank" rel="noreferrer">Kopia help docs</a> for more information on both these options. 
+            </Row>
+            <Row>
+                &nbsp;
+            </Row>
+            <Row>
                 {RequiredField(this, "GCS Bucket", "bucket", { autoFocus: true, placeholder: "enter bucket name" })}
                 {OptionalField(this, "Object Name Prefix", "prefix", { placeholder: "enter object name prefix or leave empty", type: "password" })}
             </Row>
