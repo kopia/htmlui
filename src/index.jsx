61d41af62056454e35ab7962f046857a7760ff33
Christoph Anderson <lupusapps@gmail.com>
2023-07-08 10:13:37 +0200

    Fixed some colors and the policy editor. The editor should now have the correct theme settings.
    Moved index.css to css folder.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './css/index.css';
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />);
