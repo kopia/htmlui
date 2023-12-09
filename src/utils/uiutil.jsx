46df4cadea6f2a3fe3fdc2d5772ef2e30e8eeb09
lupusA <lupusapps@gmail.com>
2023-12-09 11:48:28 +0100

    fix: Tab snapshot, policies, and tasks are disabled when repo is not connected.

--- src/utils/uiutil.jsx
+++ src/utils/uiutil.jsx
@@ -1,4 +1,4 @@
-import { faBan, faCheck, faChevronLeft, faCopy, faExclamationCircle, faExclamationTriangle, faFolderOpen, faRectangleXmark, faTerminal} from '@fortawesome/free-solid-svg-icons';
+import { faBan, faCheck, faChevronLeft, faCopy, faExclamationCircle, faExclamationTriangle, faFolderOpen, faTerminal, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import React, { useState } from 'react';
@@ -274,12 +274,10 @@ export function taskStatusSymbol(task) {
         case "RUNNING":
             return <>
                 <Spinner animation="border" variant="primary" size="sm" /> Running for {dur}
-                &nbsp;
-                <button className="btn btn-link" type="button" onClick={() => cancelTask(task.id)}>
-                    <FontAwesomeIcon color="red" title="Cancel task" size='sm' icon={faRectangleXmark} />
+                <button className="btn btn-sm btn-link" type="button" onClick={() => cancelTask(task.id)}>
+                    <FontAwesomeIcon color="red" size='lg' title="Cancel task" icon={faXmark} />
                 </button>
-            </>;
-
+            </>
         case "SUCCESS":
             return <p title={dur}><FontAwesomeIcon icon={faCheck} color="green" /> Finished in {dur}</p>;
 
