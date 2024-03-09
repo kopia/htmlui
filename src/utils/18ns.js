bc26ddad2737de2eb15e8b0c31189e458f33bedc
lupusA <lupusapps@gmail.com>
2024-03-09 17:45:59 +0100

    Some smaller changes

--- src/utils/18ns.js
+++ src/utils/18ns.js
@@ -1,17 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import backend from "i18next-http-backend";
+import languageDetector from 'i18next-browser-languagedetector';
+import backend from "i18next-http-backend"
 
 i18n
   .use(backend)
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en-GB",
+    deubg: true, 
     ns: ["translation"],
     defaultNS: "translation",
     interpolation: {
       escapeValue: false,
     },
+    react: {
+      wait: true
+    }
   });
 
 export default i18n;
