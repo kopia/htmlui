93d90d0ea8ff4876d4717280e9c6613d6154c068
blenderfreaky <blenderfreaky@mailbox.org>
2025-04-17 22:28:59 +0200

    feat(ui): allow creating per-host, per-user and foreign policies

--- src/utils/uiutil.jsx
+++ src/utils/uiutil.jsx
@@ -350,6 +350,39 @@ export function isAbsolutePath(p) {
     return false;
 }
 
+// Refer to kopia/snapshot/source.go:ParseSourceInfo
+export function checkPolicyPath(path) {
+    if (path === "(global)") {
+        return "Cannot create the global policy, it already exists.";
+    }
+
+    // Check for a path before anything else and short-circuit
+    // On Windows this avoids issues with the colon in C:/path
+    if (isAbsolutePath(path)) {
+        return null;
+    }
+
+    const p1 = path.indexOf("@");
+    const p2 = path.indexOf(":");
+
+    // user@host:path
+    if (p1 > 0 && p2 > 0 && p1 < p2 && p2 < path.length) {
+        path = path.substring(p2 + 1);
+    } else if (p1 >= 0 && p2 < 0) {
+        if (p1 + 1 < path.length) {
+            // @host and user@host without path
+            return null;
+        }
+
+        return "Policies must have a hostname.";
+    }
+
+    // We already know it isn't an absolute path,
+    // nor is it a fully specified policy target,
+    // so it's either completely invalid, or a relative path
+    return "Policies can not be defined for relative paths.";
+}
+
 export function errorAlert(err, prefix) {
     if (!prefix) {
         prefix = "Error"
