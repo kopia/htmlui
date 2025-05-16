kopia/htmlui

commit e2c12267c6db0f2ed2f359c3567ba5655be915fe

build(deps): upgraded react, react-dom an @types/react to the latest versions (#317)
* build(deps): upgraded react, react-dom an @types/react to the latest versions

* switched to a new react-table implementation

src/pages/SnapshotHistory.jsx
@@ -331,53 +331,53 @@ export class SnapshotHistory extends Component {
 
         const columns = [{
             id: 'selected',
-            Header: 'Selected',
+            header: 'Selected',
             width: 20,
             align: "center",
-            Cell: x => <div className="form-check multiselect"><input type="checkbox" className="form-check-input" checked={this.isSelected(x.row.original)} onChange={() => this.toggleSelected(x.row.original)} /></div>,
+            cell: x => <div className="form-check multiselect"><input type="checkbox" className="form-check-input" checked={this.isSelected(x.row.original)} onChange={() => this.toggleSelected(x.row.original)} /></div>,
         }, {
             id: 'startTime',
-            Header: 'Start time',
+            header: 'Start time',
             width: 200,
-            accessor: x => {
-                let timestamp = rfc3339TimestampForDisplay(x.startTime);
-                return <Link to={objectLink(x.rootID, timestamp, { label: path })}>{timestamp}</Link>;
+            cell: x => {
+                let timestamp = rfc3339TimestampForDisplay(x.row.original.startTime);
+                return <Link to={objectLink(x.row.original.rootID, timestamp, { label: path })}>{timestamp}</Link>;
             },
         }, {
             id: 'description',
-            Header: '',
+            header: '',
             width: 20,
-            Cell: x => this.descriptionFor(x.row.original),
+            cell: x => this.descriptionFor(x.row.original),
         }, {
             id: 'rootID',
-            Header: 'Root',
+            header: 'Root',
             width: "",
-            accessor: x => x.rootID,
-            Cell: x => <><span className="snapshot-hash">{x.cell.value}</span>
+            accessorFn: x => x.rootID,
+            cell: x => <><span className="snapshot-hash">{x.cell.getValue()}</span>
                 {x.row.original.description && <div className='snapshot-description'><small>{x.row.original.description}</small></div>}</>,
         }, {
-            Header: 'Retention',
+            header: 'Retention',
             accessor: 'retention',
             width: "",
-            Cell: x => <span>
-                {x.cell.value.map(l => <React.Fragment key={l}><Badge bg={"retention-badge-"+pillVariant(l)}>{l}</Badge>{' '}</React.Fragment>)}
+            cell: x => <span>
+                {/* {x.cell.getValue().map(l => <React.Fragment key={l}><Badge bg={"retention-badge-"+pillVariant(l)}>{l}</Badge>{' '}</React.Fragment>)} */}
                 {x.row.original.pins.map(l => <React.Fragment key={l}>
                     <Badge bg="snapshot-pin" onClick={() => this.editPin(x.row.original, l)}><FontAwesomeIcon icon={faThumbtack} /> {l}</Badge>{' '}
                 </React.Fragment>)}
                 {this.newPinFor(x.row.original)}
             </span>
         }, {
-            Header: 'Size',
-            accessor: 'summary.size',
+            header: 'Size',
+            accessorFn: x => x.summary.size,
             width: 100,
-            Cell: x => sizeWithFailures(x.cell.value, x.row.original.summary, bytesStringBase2),
+            cell: x => sizeWithFailures(x.cell.getValue(), x.row.original.summary, bytesStringBase2),
         }, {
-            Header: 'Files',
-            accessor: 'summary.files',
+            header: 'Files',
+            accessorFn: x => x.summary.files,
             width: 100,
         }, {
-            Header: 'Dirs',
-            accessor: 'summary.dirs',
+            header: 'Dirs',
+            accessorFn: x => x.summary.dirs,
             width: 100,
         }]
 

src/components/DirectoryItems.jsx
@@ -37,30 +37,30 @@ export class DirectoryItems extends Component {
         const { bytesStringBase2 } = this.context;
         const columns = [{
             id: "name",
-            Header: 'Name',
+            header: 'Name',
             width: "",
-            accessor: x => directoryLinkOrDownload(x, this.props.historyState),
+            cell: x => directoryLinkOrDownload(x.row.original, this.props.historyState),
         }, {
             id: "mtime",
-            accessor: "mtime",
-            Header: "Last Modification",
+            accessorFn: x => x.mtime,
+            header: "Last Modification",
             width: 200,
-            Cell: x => rfc3339TimestampForDisplay(x.cell.value),
+            cell: x => rfc3339TimestampForDisplay(x.cell.getValue()),
         }, {
             id: "size",
-            accessor: x => sizeInfo(x),
-            Header: "Size",
+            accessorFn: x => sizeInfo(x),
+            header: "Size",
             width: 100,
-            Cell: x => sizeWithFailures(x.cell.value, x.row.original.summ, bytesStringBase2),
+            cell: x => sizeWithFailures(x.cell.getValue(), x.row.original.summ, bytesStringBase2),
         }, {
             id: "files",
-            accessor: "summ.files",
-            Header: "Files",
+            accessorFn: x => x.summ ? x.summ.files : undefined,
+            header: "Files",
             width: 100,
         }, {
             id: "dirs",
-            accessor: "summ.dirs",
-            Header: "Directories",
+            accessorFn: x => x.summ ? x.summ.dirs : undefined,
+            header: "Directories",
             width: 100,
         }]
 

src/css/App.css
@@ -474,3 +474,7 @@ div.tab-body {
   font-weight: normal;
 
 }
+
+.cursor-pointer {
+  cursor: pointer;
+}

src/pages/Policies.jsx
@@ -230,24 +230,24 @@ export class Policies extends Component {
 
 
         const columns = [{
-            Header: 'Username',
+            header: 'Username',
             width: 100,
-            accessor: x => x.target.userName || "*",
+            accessorFn: x => x.target.userName || "*",
         }, {
-            Header: 'Host',
+            header: 'Host',
             width: 100,
-            accessor: x => x.target.host || "*",
+            accessorFn: x => x.target.host || "*",
         }, {
-            Header: 'Path',
-            accessor: x => x.target.path || "*",
+            header: 'Path',
+            accessorFn: x => x.target.path || "*",
         }, {
-            Header: 'Defined',
-            accessor: x => this.policySummary(x),
+            header: 'Defined',
+            cell: x => this.policySummary(x.row.original),
         }, {
             id: 'edit',
-            Header: 'Actions',
+            header: 'Actions',
             width: 50,
-            Cell: x => <Button data-testid="edit-policy" as={Link} to={policyEditorURL(x.row.original.target)} variant="primary" size="sm">Edit</Button>
+            cell: x => <Button data-testid="edit-policy" as={Link} to={policyEditorURL(x.row.original.target)} variant="primary" size="sm">Edit</Button>
         }]
 
         return <>

src/pages/Snapshots.jsx
@@ -108,7 +108,7 @@ export class Snapshots extends Component {
      * @returns - the header of the cell
      */
     setHeader(x) {
-        switch (x.cell.value) {
+        switch (x.cell.getValue()) {
             case "IDLE":
             case "PAUSED":
                 return x.cell.column.Header = "Actions"
@@ -127,7 +127,7 @@ export class Snapshots extends Component {
      */
     statusCell(x, parent, bytesStringBase2) {
         this.setHeader(x)
-        switch (x.cell.value) {
+        switch (x.cell.getValue()) {
             case "IDLE":
             case "PAUSED":
                 return <>
@@ -194,7 +194,7 @@ export class Snapshots extends Component {
     }
 
     nextSnapshotTimeCell(x, parent) {
-        if (!x.cell.value) {
+        if (!x.cell.getValue()) {
             if (x.row.original.status === "PAUSED") {
                 return "paused";
             }
@@ -206,8 +206,8 @@ export class Snapshots extends Component {
             return "";
         }
 
-        return <p title={moment(x.cell.value).toLocaleString()}>{moment(x.cell.value).fromNow()}
-            {moment(x.cell.value).isBefore(moment()) && <>
+        return <p title={moment(x.cell.getValue()).toLocaleString()}>{moment(x.cell.getValue()).fromNow()}
+            {moment(x.cell.getValue()).isBefore(moment()) && <>
                 &nbsp;
                 <Badge bg="secondary">overdue</Badge>
             </>}
@@ -248,8 +248,8 @@ export class Snapshots extends Component {
 
         const columns = [{
             id: 'path',
-            Header: 'Path',
-            accessor: x => x.source,
+            header: 'Path',
+            accessorFn: x => x.source,
             sortType: (a, b) => {
                 const v = compare(a.original.source.path, b.original.source.path);
                 if (v !== 0) {
@@ -259,38 +259,38 @@ export class Snapshots extends Component {
                 return compare(ownerName(a.original.source), ownerName(b.original.source));
             },
             width: "",
-            Cell: x => <Link to={'/snapshots/single-source?' + sourceQueryStringParams(x.cell.value)}>{x.cell.value.path}</Link>,
+            cell: x => <Link to={'/snapshots/single-source?' + sourceQueryStringParams(x.cell.getValue())}>{x.cell.getValue().path}</Link>,
         }, {
             id: 'owner',
-            Header: 'Owner',
-            accessor: x => x.source.userName + '@' + x.source.host,
+            header: 'Owner',
+            accessorFn: x => x.source.userName + '@' + x.source.host,
             width: 250,
         }, {
             id: 'lastSnapshotSize',
-            Header: 'Size',
+            header: 'Size',
             width: 120,
-            accessor: x => x.lastSnapshot ? x.lastSnapshot.stats.totalSize : 0,
-            Cell: x => sizeWithFailures(
-                x.cell.value,
+            accessorFn: x => x.lastSnapshot ? x.lastSnapshot.stats.totalSize : 0,
+            cell: x => sizeWithFailures(
+                x.cell.getValue(),
                 x.row.original.lastSnapshot && x.row.original.lastSnapshot.rootEntry ? x.row.original.lastSnapshot.rootEntry.summ : null, bytesStringBase2),
         }, {
             id: 'lastSnapshotTime',
-            Header: 'Last Snapshot',
+            header: 'Last Snapshot',
             width: 160,
-            accessor: x => x.lastSnapshot ? x.lastSnapshot.startTime : null,
-            Cell: x => x.cell.value ? <p title={moment(x.cell.value).toLocaleString()}>{moment(x.cell.value).fromNow()}</p> : '',
+            accessorFn: x => x.lastSnapshot ? x.lastSnapshot.startTime : null,
+            cell: x => x.cell.getValue() ? <p title={moment(x.cell.getValue()).toLocaleString()}>{moment(x.cell.getValue()).fromNow()}</p> : '',
         }, {
             id: 'nextSnapshotTime',
-            Header: 'Next Snapshot',
+            header: 'Next Snapshot',
             width: 160,
-            accessor: x => x.nextSnapshotTime,
-            Cell: x => this.nextSnapshotTimeCell(x, this),
+            accessorFn: x => x.nextSnapshotTime,
+            cell: x => this.nextSnapshotTimeCell(x, this),
         }, {
             id: 'status',
-            Header: '',
+            header: '',
             width: 300,
-            accessor: x => x.status,
-            Cell: x => this.statusCell(x, this, bytesStringBase2)
+            accessorFn: x => x.status,
+            cell: x => this.statusCell(x, this, bytesStringBase2)
         }]
 
         return <>

src/pages/Tasks.jsx
@@ -104,23 +104,23 @@ export class Tasks extends Component {
         }
 
         const columns = [{
-            Header: 'Start Time',
+            header: 'Start Time',
             width: 160,
-            accessor: x => <Link to={'/tasks/' + x.id} title={moment(x.startTime).toLocaleString()}>
-                {moment(x.startTime).fromNow()}
+            cell: x => <Link to={'/tasks/' + x.row.original.id} title={moment(x.row.original.startTime).toLocaleString()}>
+                {moment(x.row.original.startTime).fromNow()}
             </Link>
         }, {
-            Header: 'Status',
+            header: 'Status',
             width: 240,
-            accessor: x => taskStatusSymbol(x),
+            cell: x => taskStatusSymbol(x.row.original),
         }, {
-            Header: 'Kind',
+            header: 'Kind',
             width: "",
-            accessor: x => <p>{x.kind}</p>,
+            cell: x => <p>{x.row.original.kind}</p>,
         }, {
-            Header: 'Description',
+            header: 'Description',
             width: "",
-            accessor: x => <p>{x.description}</p>,
+            cell: x => <p>{x.row.original.description}</p>,
         }]
 
         const filteredItems = this.filterItems(items)

src/utils/KopiaTable.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Pagination from 'react-bootstrap/Pagination';
 import Table from 'react-bootstrap/Table';
-import { usePagination, useSortBy, useTable } from 'react-table';
+import Button from 'react-bootstrap/Table';
+import { useReactTable, getCoreRowModel, getPaginationRowModel, getSortedRowModel, flexRender } from '@tanstack/react-table';
 import { PAGE_SIZES, UIPreferencesContext } from '../contexts/UIPreferencesContext';
 
 function paginationItems(count, active, gotoPage) {
@@ -48,99 +49,114 @@ function paginationItems(count, active, gotoPage) {
 
 export default function KopiaTable({ columns, data }) {
   const { pageSize, setPageSize } = useContext(UIPreferencesContext);
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0, //default page index
+    pageSize: pageSize, //default page size
+  });
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    page,
-    prepareRow,
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize: setTablePageSize,
-    state: { pageIndex },
-  } = useTable({
-    columns,
+  const table = useReactTable({
     data,
-    initialState: { pageSize },
-    autoResetPage: false,
-    autoResetSortBy: false,
-  },
-    useSortBy,
-    usePagination,
-  )
+    columns,
+    state: {
+      sorting,
+      pagination,
+    },
+    autoResetPageIndex: false,
+    enableSortingRemoval: false,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(), //load client-side pagination code
+    getSortedRowModel: getSortedRowModel(),
+    onPaginationChange: setPagination, //update the pagination state when internal APIs mutate the pagination state
+    onSortingChange: setSorting,
+  })
 
-  useEffect(() => {
-    setTablePageSize(pageSize);
-  }, [pageSize, setTablePageSize]);
-
-  if (pageIndex >= pageCount && pageIndex !== 0 && pageCount > 0) {
-    gotoPage(pageCount - 1);
+  if (pagination.pageIndex >= table.getPageCount() && pagination.pageIndex !== 0) {
+    table.resetPageIndex();
   }
 
   const paginationUI = <>
-    <>{pageOptions.length > 1 && (
-      <Pagination size="sm" variant="dark">
-        <Pagination.First onClick={() => gotoPage(0)} disabled={!canPreviousPage} />
-        <Pagination.Prev onClick={() => previousPage()} disabled={!canPreviousPage} />
-        {paginationItems(pageOptions.length, pageIndex + 1, gotoPage)}
-        <Pagination.Next onClick={() => nextPage()} disabled={!canNextPage} />
-        <Pagination.Last onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage} />
-      </Pagination>)}
-    </>
-    <>
-      <Dropdown style={{ marginBottom: '1em' }}>
-        <Dropdown.Toggle size="sm">
-          Page Size: {pageSize}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {PAGE_SIZES.map(pageSize => (
-            <Dropdown.Item size="sm" key={pageSize} onClick={() => setPageSize(pageSize)}>
-              Page Size {pageSize}
-            </Dropdown.Item>))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
-  </>;
+  <>{table.getPageCount() > 1 && (
+    <Pagination size="sm" variant="dark">
+      <Pagination.First onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()} />
+      <Pagination.Prev onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()} />
+      {paginationItems(table.getPageCount(), pagination.pageIndex + 1, table.setPageIndex)}
+      <Pagination.Next onClick={() => table.nextPage()} disabled={!table.getCanNextPage()} />
+      <Pagination.Last onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()} />
+    </Pagination>)}
+  </>
+  <>
+    <Dropdown style={{ marginBottom: '1em' }}>
+      <Dropdown.Toggle size="sm">
+        Page Size: {pageSize}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {PAGE_SIZES.map(pageSize => (
+          <Dropdown.Item size="sm" key={pageSize} onClick={() => {
+            table.setPageSize(pageSize);
+            setPageSize(pageSize);
+          }}>
+            Page Size {pageSize}
+          </Dropdown.Item>))}
+      </Dropdown.Menu>
+    </Dropdown>
+  </>
+</>;
 
-  return (
-    <>
-      <Table size="sm" striped bordered hover {...getTableProps()}>
+  return <>
+    <div className="p-2">
+      <Table size="sm" striped bordered hover>
         <thead className="table-dark">
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps({
-                  ...column.getSortByToggleProps(), style: {
-                    width: column.width,
-                  }
-                })}>{column.render('Header')}
-                  {column.isSorted ? (column.isSortedDesc ? '🔽' : '🔼') : ''}
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  <div
+                        className={
+                          header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : ''
+                        }
+                        onClick={header.column.getToggleSortingHandler()}
+                        title={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === 'asc'
+                              ? 'Sort ascending'
+                              : header.column.getNextSortingOrder() === 'desc'
+                                ? 'Sort descending'
+                                : 'Clear sort'
+                            : undefined
+                        }
+                      >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  {{
+                      asc: ' 🔼',
+                      desc: ' 🔽',
+                    }[header.column.getIsSorted()] ?? null}
+                    </div>
                 </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} key={i}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          }
-          )}
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
-      </Table>
+      </Table> 
       {paginationUI}
-    </>
-  )
+    </div>
+    </>;
 }

commit 5a52270249462e8d392b21923cc49e02a3a05d33

fix(htmui): restored retention labels
As reported by @dcog989 in https://github.com/kopia/kopia/pull/4565
This was broken by https://github.com/kopia/htmlui/pull/317

src/pages/SnapshotHistory.jsx
@@ -357,10 +357,10 @@ export class SnapshotHistory extends Component {
                 {x.row.original.description && <div className='snapshot-description'><small>{x.row.original.description}</small></div>}</>,
         }, {
             header: 'Retention',
-            accessor: 'retention',
+            accessorFn: x => x.retention,
             width: "",
             cell: x => <span>
-                {/* {x.cell.getValue().map(l => <React.Fragment key={l}><Badge bg={"retention-badge-"+pillVariant(l)}>{l}</Badge>{' '}</React.Fragment>)} */}
+                {x.cell.getValue().map(l => <React.Fragment key={l}><Badge bg={"retention-badge-"+pillVariant(l)}>{l}</Badge>{' '}</React.Fragment>)}
                 {x.row.original.pins.map(l => <React.Fragment key={l}>
                     <Badge bg="snapshot-pin" onClick={() => this.editPin(x.row.original, l)}><FontAwesomeIcon icon={faThumbtack} /> {l}</Badge>{' '}
                 </React.Fragment>)}