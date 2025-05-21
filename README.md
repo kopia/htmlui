ae5b3cbde1d8effbc9467e00dc5d9e5e1e58e9be
Jarek Kowalski <jaak@jkowalski.net>
2025-05-20 20:55:02 -0700

    chore(ci): globally apply prettier formatting

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ You can use this script like this one. Here it's assumed that you cloned the ser
   cd ../kopia
   # start kopia server with default repo:
   #go run . server start --insecure --without-password --disable-csrf-token-checks --log-level=debug
-  
-  # for development you might want to use another repository for testing only, not your normal one, so you could use, e.g. 
+
+  # for development you might want to use another repository for testing only, not your normal one, so you could use, e.g.
   go run . server start --insecure --without-password --disable-csrf-token-checks --log-level=debug  --config-file=$HOME/.config/kopia/disabled/repository-dev.config
 )
 ```
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,32 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
-  {languageOptions: { globals: globals.jest }},
-  { rules: {
-    "@typescript-eslint/no-unused-vars": [
-            "error",
-            { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-        ],
-    }
+  { languageOptions: { globals: globals.jest } },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
   },
   pluginReact.configs.flat.recommended,
 ]);
